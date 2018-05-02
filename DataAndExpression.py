d39dd5d32befaf10772912d7f3c04b28066c3940
1508108406@qq.com <wu4355627>
2018-05-01 19:07:37 -0700

    Python builtin constants

---
+++ DataAndExpression.py
@@ -0,0 +1,12 @@
+import math
+
+#Pthon的内置基本类型
+
+def f():
+    print("This is a usr_defined function")
+    return 42
+
+print("Some basic types in Python.")
+print(type(2)) #int
+print(type(2.2)) #float
+print((type("2.2") #string
--- WritingFunctions.py
+++ WritingFunctions.py
@@ -16,7 +16,7 @@ print(xor(True,False)) # True
 print(xor(False,True)) #True
 
 
-#函数内调用其他函数
+#函数内调用其他函数 1
 def f(w):
     return 10*w
 
@@ -27,6 +27,74 @@ def h(z):
     return f(g(z,f(z+1)))
 
 print(h(1))
+
+#函数内调用其他函数 2
+def oneDiget(n):
+    return n%10
+
+def largetOnesDiget(x,y):
+    return max(oneDiget(x),oneDiget(y))
+
+print(largetOnesDiget(134,694))
+print(largetOnesDiget(132,674))
+
+#单元测试函数
+def oneDivision(n):
+    return n%10
+    
+def testOneDivision():
+    print("Testing oneDivision()...",end="")
+    assert(oneDivision(5) == 5)
+    assert(oneDivision(123) ==3)
+    assert(oneDivision(100) == 1)
+    print("Passed!")
+
+testOneDivision()   #报错，断言失败
+
+#局部变量
+def f(x):
+    print("In f,x= ",x)
+    x+=5
+    return x
+
+def g(x):
+    return f(x*2)+f(x*3)
+
+print(g(2))
+
+#全局变量,一般情况下避免使用全局变量
+g = 100
+
+def f(x):
+    return x+g
+
+print(f(5))
+print(f(6))
+print(g)
+
+#修改全局变量的值
+
+g=100
+
+def f(x):
+    #当修改一个全局变量的值时，需要声明它是一个全局变量，否则python会认为它是一个局部变量
+    global g
+    g += 1
+    return x+g
+
+print(f(5))
+print(f(6))
+print(g)
+
+
+#默认变量
+
+#可以在定义函数的时候定义一个变量，调用函数时缺省对应的变量可以使用默认值，否则使用传入的变量
+def f(x,y=10):
+    return x+y
+
+print(f(5))
+print(f(5,1))
 
 
 #None return
